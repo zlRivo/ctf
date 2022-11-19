c0d04412d5ba8fc598cc3cc5bcabe17a73284c52
zlRivo <tetjicraft@gmail.com>
2022-11-19 16:47:53 +0100

    challenges.re 8

--- challenges.re/ex8.c
+++ challenges.re/ex8.c
@@ -1,11 +1,32 @@
 #include <stdio.h>
 
-int func(int(*)() callback, void* arg2, void* arg3) {
-    int func_ret = (*callback)(arg3, *arg2)
-    if (func_ret == 0)
-        return arg2;
-    
-    
+// Binary Search Tree traversal algorithm with arbitrary function
+void* func(int (*callback)(int, void*), void** arg2, int arg3) {
+    if (arg2 == NULL)
+        return NULL;
+
+    while (1) {
+
+        int callback_ret = callback(arg3, *arg2);
+
+        if (callback_ret == 0) {
+
+            return arg2;
+            
+        } else if (callback_ret >= 0) {
+
+            arg2 = arg2[6];
+            if (arg2 == NULL)
+                return NULL;
+
+        } else {
+
+            arg2 = arg2[4];
+            if (arg2 == NULL)
+                return NULL;
+            
+        }
+    }
 }
 
 int main(int argc, char** argv) {
