b56e70a8ce5ee1d5125ee77da198a7d9bb49177c
zlRivo <tetjicraft@gmail.com>
2022-11-28 10:02:59 +0100

    ......

---
+++ challenges.re/ex27.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int f(int a, int b) {
+    int val = (8 * (-968154503) + 8) >> 9;
+    return ((unsigned)val >> 31) + val;
+}
+
+int main(int argc, char** argv) {
+    return 0;
+}
