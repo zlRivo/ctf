574bd346c68310e8154abb8509e5264a7811c912
zlRivo <tetjicraft@gmail.com>
2022-11-29 14:37:55 +0100

    ex28 challenges.re almost finished

--- challenges.re/ex28.c
+++ challenges.re/ex28.c
@@ -14,11 +14,25 @@ int compare(int* a, int* b) {
 }
 
 void* my_memdup(size_t size, void* addr) {
-    void* ptr = malloc(size);
-    return memcpy(ptr, addr, size);
+    return memcpy(malloc(size), addr, size);
 } 
 
-int f_main(void* a, )
+int f_main(void* a, size_t b) {
+    size_t size = b * 4;
+    
+    int* ptr = memcpy(malloc(size), a, size);
+    qsort(ptr, b, 4, compare);
+
+    b = b >> 1;
+
+    if (b & 1 == 0) { // b % 2 == 0
+        int val = ptr[b] + ptr[b - 1];
+        
+        return ((val >> 31) + val) >> 1;
+    }
+
+    return ptr[b];
+}
 
 int main(int argc, char** argv) {
     return 0;
